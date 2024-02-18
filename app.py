19993f72f18aa642bd2ffb07bdf4f0761ca907f8
ossianhempel <ossian.hempel@groupm.com>
2024-02-18 11:32:43 +0100

    feat: create main.py for entry point in deployment

--- app.py
+++ app.py
@@ -35,6 +35,5 @@ def predict():
         return render_template('home.html', result=formatted_result)
     
 if __name__ == '__main__':
-    #app.run(host="0.0.0.0", port=8080, debug=True)
-    app.run(host="0.0.0.0", port=8080, debug=False)
+    app.run(host="0.0.0.0", port=8080, debug=True) # for dev purposes
     # http://127.0.0.1:8080/predict
