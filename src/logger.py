3b9bf9b9261c10a4ecb50bdd9226b800b797476b
ossianhempel <hemposse@hotmail.com>
2024-03-11 21:10:39 +0100

    fix: change logger so it doesnt create a new directory for every log

--- src/logger.py
+++ src/logger.py
@@ -2,14 +2,15 @@ import logging
 import os
 from datetime import datetime
 
+# Define log directory and file
+log_dir = os.path.join(os.getcwd(), "logs")
+os.makedirs(log_dir, exist_ok=True)  # Ensure the directory exists
 LOG_FILE = f"{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
-os.makedirs(logs_path, exist_ok=True)
+LOG_FILE_PATH = os.path.join(log_dir, LOG_FILE)
 
-LOG_FILE_PATH=os.path.join(logs_path, LOG_FILE)
-
+# Basic logging configuration
 logging.basicConfig(
-  filename=LOG_FILE_PATH,
-  format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-  level=logging.INFO,
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(levelname)s %(name)s - %(message)s",
+    level=logging.INFO,
 )
